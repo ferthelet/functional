617396f03bf565ef709a8b038bf37b13944e952f
Juan Salazar <ferthelet@gmail.com>
2023-04-13 10:33:51 -0500

    does lab

--- Ch02/02_03/Start/examples.js
+++ Ch02/02_03/Start/examples.js
@@ -2,3 +2,22 @@ const double = x => x * 2;
 const subtractOne = x => x - 1;
 const triple = x => x * 3;
 const add5 = x => x + 5;
+
+const myNumber = 10;
+
+const doubled = double(myNumber);
+const minusOne = subtractOne(doubled);
+/// and so on
+console.log(myNumber, doubled, minusOne);
+
+const functionsArray = [
+    double,
+    subtractOne,
+    triple,
+    add5,
+];
+
+let number = 10;
+
+functionsArray.forEach(func => console.log(number + func(number)));
+console.log(number);
